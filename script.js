215f532e0eaf278cb74bca3daf6769c0d38f39c2
VirgiD <virginiadanitz@gmail.com>
2023-02-26 18:06:25 -0300

    subiendoCV

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+document.getElementById("boton").onclick=
+    function cambiar(){       
+        document.getElementById("imagenLaptop").src="imagenes/certificado1.jpg";
+       
+      };
+
+document.getElementById("boton2").onclick=      
+    function restaurar(){ 
+      document.getElementById("imagenLaptop").src="imagenes/laptop-working-in-living-room-at-home.jpg"; 
+   } ;
+
+
