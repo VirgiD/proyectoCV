248763563b2d7e40d3b8b742c50fcbe1c2f49dbc
Virginia Danitz <88666656+VirgiD@users.noreply.github.com>
2023-03-02 20:29:17 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 
 
-document.getElementById("boton").onclick=
-    function cambiar(){       
-        document.getElementById("imagenLaptop").src="imagenes/certificado1.jpg";
-       
+document.getElementById("botonA").onclick=
+    function cambiar(){
+       document.getElementById("imagenLaptop").src="imagenes/certificado1.jpg";
+         
+      };       
+document.getElementById("botonB").onclick=
+      function cambiar(){
+         document.getElementById("imagenLaptop").src="imagenes/certificado2.jpg";
+      };              
+document.getElementById("botonC").onclick=
+      function cambiar(){
+         document.getElementById("imagenLaptop").src="imagenes/certificado3.jpg";
       };
+document.getElementById("botonD").onclick=
+      function cambiar(){
+         document.getElementById("imagenLaptop").src="imagenes/certificado4.jpg";
+      };      
+document.getElementById("boton").onclick=      
+    function restaurar() {
+    document.getElementById("imagenLaptop").src = "imagenes/laptop-working-in-living-room-at-home.jpg";
+  } ;
 
-document.getElementById("boton2").onclick=      
-    function restaurar(){ 
-      document.getElementById("imagenLaptop").src="imagenes/laptop-working-in-living-room-at-home.jpg"; 
-   } ;
 
 
